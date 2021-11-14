a30cc7173a00f1f4c014ce469278bf35eb44bd45
Yonghwi Kim <yh0921k@gmail.com>
2021-11-14 10:11:06 +0900

    add: Hello RxJava 작성

---
+++ src/main/java/kyh/rxjava/HelloRxJava.java
@@ -0,0 +1,10 @@
+package kyh.rxjava;
+
+import io.reactivex.Observable;
+
+public class HelloRxJava {
+  public static void main(String[] args) {
+    Observable<String> observable = Observable.just("Hello", "RxJava");
+    observable.subscribe(data -> System.out.println(data));
+  }
+}

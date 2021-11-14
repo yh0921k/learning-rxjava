dc4eba87b92a8588e841ba5b25b14824205d2aa0
Yonghwi Kim <yh0921k@gmail.com>
2021-11-14 20:25:36 +0900

    add: BackpressureStrategy - DROP 전략

---
+++ src/main/java/kyh/rxjava/chapter03/s02/BackpressureDropExample.java
@@ -0,0 +1,28 @@
+package kyh.rxjava.chapter03.s02;
+
+import io.reactivex.BackpressureOverflowStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import kyh.rxjava.utils.LogType;
+import kyh.rxjava.utils.Logger;
+import kyh.rxjava.utils.TimeUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class BackpressureDropExample {
+  public static void main(String[] args){
+    Flowable.interval(300L, TimeUnit.MILLISECONDS)
+            .doOnNext(data -> Logger.log("#inverval doOnNext()", data))
+            .onBackpressureDrop(dropData -> Logger.log(LogType.PRINT, dropData + " Drop!"))
+            .observeOn(Schedulers.computation(), false, 1)
+            .subscribe(
+                    data -> {
+                      TimeUtil.sleep(1000L);
+                      Logger.log(LogType.ON_NEXT, data);
+                    },
+                    error -> Logger.log(LogType.ON_ERROR, error)
+            );
+
+    TimeUtil.sleep(5500L);
+  }
+}
